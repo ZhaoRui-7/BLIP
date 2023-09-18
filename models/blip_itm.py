e2d4b0e5951f05277b94ecec7d6560b922af9214
ZhaoRui-7 <61312633+ZhaoRui-7@users.noreply.github.com>
2023-09-18 15:24:37 +0800

    Update blip_itm.py

--- models/blip_itm.py
+++ models/blip_itm.py
@@ -64,7 +64,7 @@ class BLIP_ITM(nn.Module):
             text_feat = F.normalize(self.text_proj(text_output.last_hidden_state[:,0,:]),dim=-1)    
             
             # sim = image_feat @ text_feat.t()
-            return text_feat,text_feat
+            return text_feat,image_feat
         
         
 def blip_itm(pretrained='',**kwargs):
